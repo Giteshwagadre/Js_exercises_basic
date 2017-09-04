421da1549c448df5daa150326d742107d885a8f1
gitesh <giteshwagadre@gmail.com>
2017-09-05 01:37:51 +0530

    add basic javascxript code

---
+++ 1_newline_character.js
@@ -0,0 +1,1 @@
+console.log("This is the first line \n And this is the second line");
---
+++ sum of first 1 00 natural numbers.js
@@ -0,0 +1,10 @@
+var i,t;
+t=0;
+document.write("First ten natural numbers are:");
+for(i=1;i<=100;i++)
+{
+ t=t+i;
+ document.write("<br>"+i);
+}
+document.write("<br>");
+document.write("Sum="+t);
